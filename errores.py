71ec407bd6bc24ac076095eafdf079e3861a2fbd
Your Name <you@example.com>
2024-04-22 16:38:15 -0600

    Errores comunes en python 22/04/24

---
+++ errores.py
@@ -0,0 +1,13 @@
+numHuevos=12
+#No es vcálido y da error print(numHuevos+"Hola")
+
+#Opción1
+print("Yo tengo"+str(numHuevos) +"grandes xd")
+
+#Opción2
+print("Tengo %s huevos"%(numHuevos))
+
+#Calcular la superficie de un cuadrado.
+lado=int(input("Ingrese la medida del lado del cuadrado"))
+superficie=lado*lado
+print("La superficie del cuadrado es:"+str(superficie))
